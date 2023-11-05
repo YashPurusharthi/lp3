7e671906def05f8bad3ca363d8f10a66f8463759
YashPurusharthi <104548569+YashPurusharthi@users.noreply.github.com>
2023-11-05 19:37:18 +0530

    Update queen.py

--- queen.py
+++ queen.py
@@ -1,48 +1,57 @@
-def is_safe(board, row, col):
-    # Check the row on the left side
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
+def solveNQueens(n):
+    board = [ ['.' for x in range(n)] for x in range(n)]
+    solutions = []
 
-    # Check upper diagonal on the left side
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
+    def isSafe(row, col):
+        
+        # Check if there is a queen in the same column
+        for i in range(row):
+            if board[i][col] == 'Q':
+                return False
 
-    # Check lower diagonal on the left side
-    for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
+            
+        # Check the upper-left diagonal
+        i = row - 1
+        j = col - 1
+        while i >= 0 and j >= 0:
+            if board[i][j] == 'Q':
+                return False
+            i = i - 1
+            j = j - 1
 
-    return True
+            
+        # Check the upper-right diagonal
+        i = row - 1
+        j = col + 1
+        while i >= 0 and j < n:
+            if board[i][j] == 'Q':
+                return False
+            i = i - 1
+            j = j + 1
 
-def solve_n_queens(board, col):
-    if col == len(board):
         return True
 
-    for i in range(len(board)):
-        if is_safe(board, i, col):
-            board[i][col] = 1
-            if solve_n_queens(board, col + 1):
-                return True
-            board[i][col] = 0  # Backtrack if placement leads to no solution
+    
+    
+    def backtrack(row):
+        if row == n:
+            # Found a valid solution, add it to the list of solutions
+            solutions.append([' '.join(row) for row in board])
+            return
 
-    return False
+        for col in range(n):
+            if isSafe(row, col):
+                board[row][col] = 'Q'
+                backtrack(row + 1)
+                board[row][col] = '.'
 
-def print_board(board):
-    for row in board:
-        print(" ".join(["Q" if cell == 1 else "-" for cell in row]))
+    backtrack(0)
+    return solutions
 
-# Create an 8x8 chessboard with all cells initialized to 0
-n = 8
-chessboard = [[0 for _ in range(n)] for _ in range(n)]
 
-# Place the first queen in the first row, first column
-chessboard[0][0] = 1
-
-# Try to solve the remaining queens
-if solve_n_queens(chessboard, 1):
-    print("Solution found:")
-    print_board(chessboard)
-else:
-    print("No solution exists.")
+solutions = solveNQueens(4)
+print("possible solution",len(solutions))
+for solution in solutions:
+    for row in solution:
+        print(row)
+    print()
