69109a426c38cbaca9e24b6abd75144fe84413cd
YashPurusharthi <104548569+YashPurusharthi@users.noreply.github.com>
2023-11-06 20:29:43 +0530

    Update knapsackdp.py

--- knapsackdp.py
+++ knapsackdp.py
@@ -1,50 +1,3 @@
-#USING BRANCH AND BOUND
-
-class Item:
-    def __init__(self, weight, value):
-        self.weight = weight
-        self.value = value
-
-def knapsack_branch_and_bound(items, capacity):
-    def bound(i, weight, value):
-        if weight > capacity:
-            return 0
-        bound_value = value
-        j = i
-        total_weight = weight
-        while j < n and total_weight + items[j].weight <= capacity:
-            total_weight += items[j].weight
-            bound_value += items[j].value
-            j += 1
-        if j < n:
-            bound_value += (capacity - total_weight) * (items[j].value / items[j].weight)
-        return bound_value
-
-    def knapsack_recursive(i, weight, value):
-        nonlocal max_value
-        if weight <= capacity and value > max_value:
-            max_value = value
-        if i < n:
-            if weight + items[i].weight <= capacity:
-                knapsack_recursive(i + 1, weight + items[i].weight, value + items[i].value)
-            if bound(i + 1, weight, value) > max_value:
-                knapsack_recursive(i + 1, weight, value)
-
-    n = len(items)
-    max_value = 0
-    items.sort(key=lambda x: x.value / x.weight, reverse=True)
-    knapsack_recursive(0, 0, 0)
-    return max_value
-
-# Example usage
-if __name__ == "__main__":
-    items = [Item(10, 60), Item(20, 100), Item(30, 120)]
-    capacity = 50
-
-    max_value = knapsack_branch_and_bound(items, capacity)
-    print("Maximum value in the knapsack:", max_value)
-
-
 #USING DYNAMIC PROGRAMMING
 def knapsack_dynamic_programming(values, weights, capacity):
     n = len(values)
@@ -75,7 +28,20 @@ if __name__ == "__main__":
     values = [60, 100, 120]
     weights = [10, 20, 30]
     capacity = 50
-
+    #values = [1,4,5,7]
+    #weights = [1,3,4,5]
+    #capacity = 7
+    #capacity = int(input("enter max capacity: "))
+    #n = int(input("enter no. of objects: "))
+    #values=[]
+    #weights=[]
+    #for i in range(n):
+        #v = int(input(f"enter value of {i}: "))
+        #w = int(input(f"enter weight {i}: "))
+        #values.append(v)
+        #weights.append(w)
+    
+    #if capacity:
     max_value, selected_items = knapsack_dynamic_programming(values, weights, capacity)
     print("Maximum value in the knapsack:", max_value)
     print("Selected items:", selected_items)
